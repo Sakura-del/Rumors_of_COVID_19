d162af42726da0d3d67ab3d776eb33267d73fb1e
Sakura-del <2653433352@qq.com>
2021-03-30 21:51:19 +0800

    数据接口测试

--- covid/current.py
+++ covid/current.py
@@ -5,12 +5,12 @@ from lib.handler import dispatcherBase
 from django.http import HttpResponse, JsonResponse
 import json
 import datetime
-from common.models import CurrentCovidInternal
+from common.models import *
 from lib.handler import dispatcherBase
 
 
 def listCurrentInternal(request):
-    data = CurrentCovidInternal.objects.values().filter(date=datetime.date.today())
+    data = CurrentCovidInternal.objects.all().filter(date=datetime.date.today()).first()
     data = model_to_dict(data)
     confirmedCount = data['confirmed']['count']
     confirmedIncr = data['confirmed']['incr']
@@ -41,10 +41,35 @@ def listCurrentInternal(request):
                          "date": date})
 
 
+def listCurrentGlobal(request):
+    data = CurrentCovidGlobal.objects.all().filter(date=datetime.date.today()).first()
+    data = model_to_dict(data)
+    confirmedCount = data['confirmed']['count']
+    confirmedIncr = data['confirmed']['incr']
+    curedCount = data['cured']['count']
+    curedIncr = data['cured']['incr']
+    currentConfirmedCount = data['current_confirmed']['count']
+    currentConfirmedIncr = data['current_confirmed']['incr']
+    deadCount = data['confirmed']['count']
+    deadIncr = data['confirmed']['incr']
+    date = datetime.date.today()
+
+    return JsonResponse({"confirmedCount": confirmedCount,
+                         "confirmedIncr": confirmedIncr,
+                         "curedCount": curedCount,
+                         "curedIncr": curedIncr,
+                         "currentConfirmedCount": currentConfirmedCount,
+                         "currentConfirmedIncr": currentConfirmedIncr,
+                         "deadCount": deadCount,
+                         "deadIncr": deadIncr,
+                         "date": date})
+
+
 
 
 ActionHandler = {
     'list_current_internal': listCurrentInternal,
+    'list_current_global': listCurrentGlobal
 }
 
 
