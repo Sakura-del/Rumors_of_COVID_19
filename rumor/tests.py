0cc21b465e3e697f67a9ca36fda5a5ab95eaeebe
Sakura-del <2653433352@qq.com>
2021-04-07 17:15:22 +0800

    接口更新

--- rumor/tests.py
+++ rumor/tests.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 # Create your tests here.
 import requests, pprint
 
-payload = {'action': 'list_covid_news'}
+payload = {'action': 'get_tag_count'}
 
-response = requests.get("http://127.0.0.1:8000/rumor/news", params=payload)
+response = requests.get("http://127.0.0.1:8000/rumor/shows", params=payload)
 
 pprint.pprint(response.json())
