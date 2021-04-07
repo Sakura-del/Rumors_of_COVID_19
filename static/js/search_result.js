f54dc0a1cbc18d2b2b51fd41d8d457a1b82faefc
lanhesanqi <lanhesanqi@whut.edu.cn>
2021-04-07 11:06:41 +0800

    新页面加载谣言查询结果

--- static/js/search_result.js
+++ static/js/search_result.js
@@ -13,8 +13,9 @@ $.ajax({
     data: { action: "get_rumors", title: input_content },
     dataType: "json",
     success: function (result) {
+        console.log(result);
         father_div.innerHTML="";
-        for (var i = 0; i < 10 && i < result["retlist"].length; i++) {
+        for (var i = 0; i < 10 && i < result["rumors"].length; i++) {
             var new_col_sm_9_div = document.createElement("div");
             new_col_sm_9_div.className = "col-sm-9";
 
@@ -22,27 +23,27 @@ $.ajax({
             var new_h3 = document.createElement("h3");
             new_h3.className="rumor_title_h3";
             // new_h3.style.fontSize = "18px";
-            new_h3.innerHTML = result["retlist"][i]["title"];
+            new_h3.innerHTML = result["rumors"][i]["title"];
             new_col_sm_9_div.appendChild(new_h3);
 
             var new_p = document.createElement("p");
             new_p.className = "text-muted";
-            new_p.innerHTML = result["retlist"][i]["date"];
+            new_p.innerHTML = result["rumors"][i]["date"];
             new_col_sm_9_div.appendChild(new_p);
 
             var new_p = document.createElement("p");
-            new_p.innerHTML = result["retlist"][i]["markstyle"];
+            new_p.innerHTML = result["rumors"][i]["markstyle"];
             new_col_sm_9_div.appendChild(new_p);
 
             var new_p = document.createElement("p");
             new_p.className = "text-muted";
             var tmptag;
-            for (var j = 0; j < result["retlist"][i]["tag"].length; j++) {
+            for (var j = 0; j < result["rumors"][i]["tag"].length; j++) {
                 if (j == 0) {
-                    tmptag = result["retlist"][i]["tag"][j];
+                    tmptag = result["rumors"][i]["tag"][j];
                 }
                 else {
-                    tmptag = tmptag + ", " + result["retlist"][i]["tag"][j];
+                    tmptag = tmptag + ", " + result["rumors"][i]["tag"][j];
                 }
             }
             new_p.innerHTML = tmptag;
@@ -52,7 +53,7 @@ $.ajax({
             var new_col_sm_3_div = document.createElement("div");
             new_col_sm_3_div.className = "col-sm-3";
             var newimg = document.createElement("img");
-            newimg.src = result["retlist"][i]["coversqual"];
+            newimg.src = result["rumors"][i]["coversqual"];
             new_col_sm_3_div.appendChild(newimg);
 
             var new_row_div = document.createElement("div");
