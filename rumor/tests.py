fe2dee8966ef20fc18430d90117eebbe30d9fed9
Sakura-del <2653433352@qq.com>
2021-04-08 18:10:13 +0800

    添加了页数，更新了接口

--- rumor/tests.py
+++ rumor/tests.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 # Create your tests here.
 import requests, pprint
 
-payload = {'action': 'get_tag_count'}
+payload = {'action': 'list_more_rumors', 'title': '美国疫情', 'pagenum': 2,'pagesize': 10}
 
-response = requests.get("http://127.0.0.1:8000/rumor/shows", params=payload)
+response = requests.get("http://127.0.0.1:8000/rumor/views", params=payload)
 
 pprint.pprint(response.json())
--- rumor/views.py
+++ rumor/views.py
@@ -3,19 +3,21 @@ import json
 from common.models import CurrentCovidInternal
 from lib.handler import dispatcherBase
 from common.models import RumorInfo
+from common.models import HeadlinesNews
 from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage
+import jieba
 
 
 # Create your views here.
 # 谣言初始化界面
 def list_rumors(request):
     try:
-        data = RumorInfo.objects.values('id', 'title', 'date', 'markstyle',
-                                        'result', 'explain', 'tag', 'videourl',
-                                        'cover', 'coverrect',
-                                        'coversqual').order_by('-date')[:5]
+        data = RumorInfo.objects.values('title', 'date', 'markstyle', 'result',
+                                        'explain', 'tag', 'videourl', 'cover',
+                                        'coverrect', 'coversqual').order_by('-date')[:10]
     except RumorInfo.DoesNotExist:
-        return JsonResponse({"ret": 1, "msg": "未查到相关信息"})
+        return JsonResponse({"ret": 1, "msg": "信息获取失败"})
     data = list(data)
 
     return JsonResponse({"ret": 0, "retlist": data, "total": len(data)})
@@ -23,32 +25,78 @@ def list_rumors(request):
 
 # 查询谣言
 def get_rumors(request):
-    title = request.params['title']
-    # qs = RumorInfo.objects.values('id', 'title', 'date', 'markstyle', 'result',
-    #                               'explain', 'tag', 'videourl', 'cover',
-    #                               'coverrect', 'coversqual').order_by('-date')
-    # keywords = request.params.get('title', None)
-    # if keywords:
-    #     conditions = [
-    #         Q(title__contains=one) for one in keywords.split(' ') if one
-    #     ]
-    #     query = Q()
-    #     for condition in conditions:
-    #         query |= condition
-    #     qs = qs.filter(query)
+    try:
+        qs = RumorInfo.objects.values('id', 'title', 'date', 'markstyle', 'result',
+                                      'explain', 'tag', 'videourl', 'cover',
+                                      'coverrect', 'coversqual').order_by('-date')
+        title = request.params['title']
+        keywords = jieba.cut(title)
+        if keywords:
+            conditions = [
+                Q(title__contains=one) for one in keywords if one
+            ]
+            query = Q()
+            for condition in conditions:
+                query |= condition
+            qs = qs.filter(query)
+            # 页数
+            pagenum = request.params['pagenum']
+            # 每页谣言数量
+            pagesize = request.params['pagesize']
+            pgnt = Paginator(qs, pagesize)
+            page = pgnt.page(pagenum)
+            rumors = list(page)
+            total_rumors = pgnt.count()
+    except EmptyPage:
+        return JsonResponse({'ret': 0, 'total_rumors': [], 'total': 0, "msg": "没有更多数据了"})
+    except RumorInfo.DoesNotExist:
+        return JsonResponse({"ret": 1, "msg": "信息获取失败"})
 
-    rumors = RumorInfo.objects.values('id', 'title', 'date', 'markstyle', 'result',
-                                    'explain', 'tag', 'videourl', 'cover',
-                                    'coverrect', 'coversqual').filter(title__contains=title)
+    try:
+        news = HeadlinesNews.objects.values(
+            'title', 'date', 'link', 'field', 'summary'
+        ).filter(query).order_by('-date')
+        pgnt = Paginator(news, pagesize)
+        page = pgnt.page(pagenum)
+        news = list(page)
+        total_news = pgnt.count()
+    except EmptyPage:
+        return JsonResponse({'ret': 0, 'total_news': [], 'total': 0, "msg": "没有更多数据了"})
+    except HeadlinesNews.DoesNotExist:
+        return JsonResponse({"ret": 1, "msg": "信息获取失败"})
 
-    rumors = list(qs)
+    return JsonResponse(
+        {'ret': 0, 'rumors': rumors, 'news': news, 'total_rumors': len(rumors), 'total_news': len(news),
+         "msg": ""})
 
-    return JsonResponse({'ret': 0, 'retlist': rumors, 'total': len(rumors)})
+
+# 加载更多谣言
+def list_more_rumors(request):
+    try:
+        qs = RumorInfo.objects.values('id', 'title', 'date', 'markstyle', 'result',
+                                      'explain', 'tag', 'videourl', 'cover',
+                                      'coverrect', 'coversqual').order_by('date')
+        # 页数
+        pagenum = request.params['pagenum']
+        # 每页谣言数量
+        pagesize = request.params['pagesize']
+
+        pgnt = Paginator(qs, pagesize)
+        page = pgnt.page(pagenum)
+        page = list(page)
+
+        return JsonResponse({"ret": 0, "retlist": page, "total": len(page), "msg": ""})
+
+    except EmptyPage:
+        return JsonResponse({'ret': 0, 'retlist': [], 'total': 0, "msg": "没有更多数据了"})
+    except RumorInfo.DoesNotExist:
+        return JsonResponse({"ret": 1, "msg": "信息获取失败"})
 
 
 ActionHandler = {
     'get_rumors': get_rumors,
     'list_rumors': list_rumors,
+    'list_more_rumors': list_more_rumors
 }
 
 
