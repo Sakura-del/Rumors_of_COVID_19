dbf0c2effc01fbe3a828e97977fe9c8d3bf03804
Mikumikumi0116 <mikumikumi0116@gmail.com>
2021-04-24 22:05:01 +0800

    疫苗可视化州名和疫苗接种点bug

--- static/js/vaccine_vaccination.js
+++ static/js/vaccine_vaccination.js
@@ -1,12 +1,12 @@
 supported_province_list = []    //有接种点的省
 supported_city_list = []
-supported_distinct_list = []
+supported_district_list = []
 agency_list = []
 
 
-function On_distinct_button_clicked(distinct_name) {
+function On_district_button_clicked(district_name) {
     for (var i = 0; i < agency_list.length; i++)
-        if (agency_list[i]['district'] == distinct_name) {
+        if (agency_list[i]['district'] == district_name) {
             var title_div = document.createElement('div')
             title_div.className = 'title_div'
             title_div.innerHTML = agency_list[i]['title']
@@ -27,7 +27,7 @@ function On_distinct_button_clicked(distinct_name) {
             agency_div.appendChild(title_div)
             agency_div.appendChild(tel_div)
             agency_div.appendChild(address_div)
-            if (i!=agency_list.length - 1)
+            if (i != agency_list.length - 1)
                 agency_div.appendChild(spacer_div)
 
             search_result_list.appendChild(agency_div)
@@ -35,36 +35,36 @@ function On_distinct_button_clicked(distinct_name) {
 }
 
 function On_city_button_clicked(city_name) {
-    var distinct_block = document.getElementById('distinct_block')
-    distinct_block.innerHTML = ''
+    var district_block = document.getElementById('district_block')
+    district_block.innerHTML = ''
 
 
 
     for (var i = 0; i < agency_list.length; i++)
         if (agency_list[i]['city'] == city_name)
-            supported_distinct_list.push(agency_list[i]['district'])
-    supported_distinct_list.sort()
+            supported_district_list.push(agency_list[i]['district'] != '' ? agency_list[i]['district'] : agency_list[i]['city'])
+    supported_district_list.sort()
 
     var temp_array = []
-    for (var i = 1; i < supported_distinct_list.length; i++)
-        if (supported_distinct_list[i] != supported_distinct_list[i - 1])
-            temp_array.push(supported_distinct_list[i])
+    for (var i = 1; i < supported_district_list.length; i++)
+        if (supported_district_list[i] != supported_district_list[i - 1])
+            temp_array.push(supported_district_list[i])
     if (temp_array.length == 0)
-        temp_array.push(supported_distinct_list[0])
-    supported_distinct_list = temp_array
+        temp_array.push(supported_district_list[0])
+    supported_district_list = temp_array
 
 
 
-    for (var i = 0; i < supported_distinct_list.length; i++) {
-        distinct_name = supported_distinct_list[i]
+    for (var i = 0; i < supported_district_list.length; i++) {
+        district_name = supported_district_list[i]
 
-        var distinct_button = document.createElement('button')
-        distinct_button.className = 'button distinct_button'
-        distinct_button.value = distinct_name
-        distinct_button.innerHTML = distinct_name
-        distinct_button.onclick = function () { On_distinct_button_clicked(this.value) }
+        var district_button = document.createElement('button')
+        district_button.className = 'button district_button'
+        district_button.value = district_name
+        district_button.innerHTML = district_name
+        district_button.onclick = function () { On_district_button_clicked(this.value) }
 
-        distinct_block.appendChild(distinct_button)
+        district_block.appendChild(district_button)
     }
 }
 
@@ -80,8 +80,8 @@ function On_province_button_clicked(province_name) {
         success: function (result) {
             var city_block = document.getElementById('city_block')
             city_block.innerHTML = ''
-            var distinct_block = document.getElementById('distinct_block')
-            distinct_block.innerHTML = ''
+            var district_block = document.getElementById('district_block')
+            district_block.innerHTML = ''
 
             for (i in result['citys']) {
                 city_name = result['citys'][i][0]['city']
@@ -96,9 +96,13 @@ function On_province_button_clicked(province_name) {
                 city_block.appendChild(city_button)
             }
 
+            console.log(result)
             for (i in result['citys'])
-                for (j in result['citys'][i])
+                for (j in result['citys'][i]) {
+                    if (result['citys'][i][j]['district'] == '')
+                        result['citys'][i][j]['district'] = result['citys'][i][j]['city']
                     agency_list.push(result['citys'][i][j])
+                }
         }
     })
 }
