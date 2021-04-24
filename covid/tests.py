dcea51158fe3821b3a986cb62ca0f30556582642
Sakura-del <2653433352@qq.com>
2021-04-24 16:26:06 +0800

    bug修复

--- covid/tests.py
+++ covid/tests.py
@@ -4,8 +4,8 @@ from django.test import TestCase
 # Create your tests here.
 import requests, pprint
 
-payload = {'action': 'list_covid_news', "field": "社会", 'pagenum': 2, 'pagesize': 10}
+payload = {'action': 'list_current_nations', "field": "社会", 'pagenum': 2, 'pagesize': 10}
 
-response = requests.get("http://127.0.0.1:8000/covid/news", params=payload)
+response = requests.get("http://127.0.0.1:8000/covid/current", params=payload)
 
 pprint.pprint(response.json())
