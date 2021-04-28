4b4c7e5a425034fb041ddcecb2fd7aca4bf7572e
Sakura-del <2653433352@qq.com>
2021-04-28 14:11:07 +0800

    数据更新

--- data_source/一键爬虫.py
+++ data_source/一键爬虫.py
@@ -45,7 +45,7 @@ def 一键爬虫():
     from 腾讯新闻疫情新闻 import func_list
     all_func_list += func_list
 
-    from 头条疫情新闻 import func_list
+    # from 头条疫情新闻 import func_list
     # all_func_list += func_list
 
     from 中国互联网联合辟谣 import func_list
